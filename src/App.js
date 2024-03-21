b6ddbbb63286fcbfc4162a45f69d8e109ef2607b
natalie.hallerdal@sensera.se <natalie.hallerdal@yahoo.se>
2024-03-21 11:42:42 +0100

    small change

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome to my site</h1>
-    
+        <h1>Welcome!</h1>
       </header>
     </div>
   );
