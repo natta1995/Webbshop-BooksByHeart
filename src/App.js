655e99d7a1817ab7a96c92ac6622a9bc78f55a98
natalie.hallerdal@sensera.se <natalie.hallerdal@yahoo.se>
2024-03-22 15:11:03 +0100

    "Remove h2"

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Welcome!</h1>
-        <h2>Welcome</h2>
         <p>This is my webbsite</p>
         <p>By: Natalie Hällerdal</p>
       </header>
