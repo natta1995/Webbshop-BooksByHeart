c3e371679191e9deaebf4dab84f8470fc3565c73
natalie.hallerdal@sensera.se <natalie.hallerdal@yahoo.se>
2024-05-02 15:38:56 +0200

    changes x2

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -6,16 +6,19 @@ const ProductPage = () => {
     
   
     return (
-      <div style={{ marginTop: "100px", display: "flex", flexWrap: "wrap", justifyContent: "center" }}>
+      <div style={{ marginTop: "100px", display: "flex", flexWrap: "wrap", justifyContent: "center"}}>
         {products.map((product) => (
-          <Link to={`/product/${product.id}`} key={product.id} style={{ textDecoration: "none", color: "inherit" }}>
-            <div style={{ width: "300px", height: "300px", margin: "10px", border: "1px solid #ccc", padding: "20px", textAlign: "center" }}>
+          <Link to={`/product/${product.id}`} key={product.id} style={{ textDecoration: "none", color: "inherit", border: "1px, solid, black",  width: "400px", margin: "10px" }}>
+            <div style={{ width: "320px", height: "400px", margin: "10px", padding: "0px", textAlign: "center" }}>
               <img src={product.image} alt={product.name} style={{ maxWidth: "100%", maxHeight: "100%", marginBottom: "10px" }} />
-              <p>{product.name}</p>
-              <p>{product.cost}</p>
+            
             </div>
+            <p>{product.name}</p>
+            <p>{product.cost}</p>
           </Link>
+    
         ))}
+          
       </div>
     );
   }
