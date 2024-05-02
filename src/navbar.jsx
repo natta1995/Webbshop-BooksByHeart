ff2cffcb744979bf49d54093b4e4cb9f74575453
natalie.hallerdal@sensera.se <natalie.hallerdal@yahoo.se>
2024-05-02 10:07:36 +0200

    "uppdating startpage"

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -6,7 +6,7 @@ function Navbar() {
     return (
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
-          <Link className="navbar-brand" to="/">Min Webbplats</Link>
+          <Link className="navbar-brand" to="/">BooksByHeart</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
