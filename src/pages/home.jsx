dd3a271c94e96836e20b7b4c09abbe7ad0df4b71
natalie.hallerdal@sensera.se <natalie.hallerdal@yahoo.se>
2024-06-17 13:53:32 +0200

    change

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -11,13 +11,12 @@ const HomePage = () => {
     
     <div style={{ position: 'relative' }}>
       <img src={babyImg} alt="babyreading" style={{ width: '100%', height: 'auto' }} />
-      <div style={{ position: "absolute", bottom: "20px", right: "20px", zIndex: 1}}>
+      <div style={{ position: "absolute", bottom: "20px", top: "900px", right: "20px", zIndex: 1}}>
       <Link to="/products" style={{ textDecoration: "none" }}>
           <Button size="lg" variant="light">Börja shoppa</Button>
         </Link>
-      
-      </div>
-        <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', zIndex: 1, color: 'white', textAlign: 'center' }}>
+        </div>
+        <div style={{ position: 'absolute', top: '20%', left: '50%', transform: 'translate(-50%, -50%)', zIndex: 1, color: 'white', textAlign: 'center' }}>
             <h1>Välkommen till BooksByHeart</h1>
             <p>Mer text om det behövs</p>
             <p>Du vet väl att vi har bokklubbar som du kan gå med i? För barn mellan 0 - 12 år! <Link to="/bookclub">Läs mer här</Link></p>
