876d53c3d6965c980046938e7a671711e9e85390
natalie.hallerdal@sensera.se <natalie.hallerdal@yahoo.se>
2024-06-17 16:33:57 +0200

    db

--- src/database.js
+++ src/database.js
@@ -11,11 +11,17 @@ import img6 from "../src/img/rabbit.jpg"
 
 const products = [
     { id: 1, name: "Nalles första snö", image: img1, cost: "69:-" },
-    { id: 2, name: "Postmannen och brevduvans galna race", image: img2, cost: "129:-" },
+    { id: 2, name: "Postmannen och brevduvans galna race emot klockan", image: img2, cost: "129:-" },
     { id: 3, name: "Max - Den lilla bruna hunden", image: img3, cost: "247:-" },
-    { id: 4, name: "I sagornas värd", image: img4, cost: "59:-" },
+    { id: 4, name: "I sagornas värd - pumpflickan", image: img4, cost: "59:-" },
     { id: 5, name: "Flickan, hunden och natten", image: img5, cost: "99:-" },
-    { id: 6, name: "Familjen kanin åker pulka", image: img6, cost: "349:-"},
+    { id: 6, name: "Familj Kanin åker pulka", image: img6, cost: "349:-"},
+    { id: 7, name: "Nalles första snö", image: img1, cost: "69:-" },
+    { id: 8, name: "Postmannen och brevduvans galna race", image: img2, cost: "129:-" },
+    { id: 9, name: "Max - Den lilla bruna hunden", image: img3, cost: "247:-" },
+    { id: 10, name: "I sagornas värd", image: img4, cost: "59:-" },
+    { id: 11, name: "Flickan, hunden och natten", image: img5, cost: "99:-" },
+    { id: 12, name: "Familjen kanin åker pulka", image: img6, cost: "349:-"},
     
   ];
 
