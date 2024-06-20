6f5c486cbef1c842a3b1a049be41f2ed36357783
natalie.hallerdal@sensera.se <natalie.hallerdal@yahoo.se>
2024-06-20 18:04:11 +0200

    css

--- src/pages/productDetail.jsx
+++ src/pages/productDetail.jsx
@@ -1,40 +1,104 @@
 import { useParams } from "react-router-dom";
 import { getProductById } from "../database";
-import { Button } from "react-bootstrap"
+import { Button } from "react-bootstrap";
+import styled from "styled-components";
+
+const Container = styled.div`
+  margin-top: 20px;
+  margin-left: 20px;
+  display: flex;
+  flex-direction: row;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    margin-left: 0;
+    align-items: center;
+  }
+`;
+
+const ImageContainer = styled.div`
+  flex: 1;
+  position: relative;
+  display: flex;
+
+  img {
+    width: 400px;
+    height: 500px;
+    border: 3px solid black;
+
+    @media (max-width: 768px) {
+      width: 90%;
+      height: auto;
+    }
+  }
+
+  div {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    background-color: rgba(0, 0, 0, 0.0);
+    color: white;
+    padding: 5%;
+    
+
+    h4, h6 {
+      margin: -20;
+      color: black;
+    }
+  }
+`;
+
+const ProductInfo = styled.div`
+  flex: 1;
+  margin-left: -35%;
+
+  @media (max-width: 768px) {
+    margin-left: 0;
+    text-align: center;
+    padding: 20px;
+  }
+
+  h1, h2, h6 {
+    padding-bottom: 0%;
+  }
+
+  h6 {
+    padding-top: 0%;
+  }
+`;
 
 const ProductDetailPage = () => {
   const { id } = useParams();
   const product = getProductById(id);
 
-
   return (
-    <div style={{ marginTop: "20px", marginLeft: "20px", display: "flex" }}>
+    <Container>
       {product && (
-        
-        <div style={{ flex: 1, position: "relative" }}>
-        <img src={product.image} alt={product.name} style={{ width: "400px", height: "500px", border: "3px solid black" }} />
-        <div style={{ position: "absolute", bottom: "0", left: "0", width: "100%", backgroundColor: "rgba(0, 0, 0, 0.0)", color: "white" , padding: "5%" }}>
-          <h4 style={{ margin: "0", color: "black" }}>{product.name}</h4>
-          <h6 style={{ margin: "0", color: "black" }}>{product.by}</h6>
-        </div>
-      </div>
+        <ImageContainer>
+          <img src={product.image} alt={product.name} />
+          <div>
+            <h4>{product.name}</h4>
+            <h6>{product.by}</h6>
+          </div>
+        </ImageContainer>
       )}
-      <div style={{ flex: 1, marginLeft: "-35%"}}>
-        <h1>{product.name}</h1>
-        <h2 style={{paddingBottom: "2%"}}>Av: {product.by}</h2>
-        <h6>Ålder: {product.age}</h6>
-        <h6>Produkt: Bok</h6>
-        <h6>Typ: {product.sort}</h6>
-        <h6 style={{paddingTop: "2%"}}>Beskrivning:</h6>
-        <h6>{product.des}</h6>
-        {product && (
+      {product && (
+        <ProductInfo>
+          <h1>{product.name}</h1>
+          <h2>Av: {product.by}</h2>
+          <h6 style={{paddingTop: "2%"}}>Ålder: {product.age}</h6>
+          <h6>Produkt: Bok</h6>
+          <h6>Typ: {product.sort}</h6>
+          <h6 style={{paddingTop: "2%"}}>Beskrivning:</h6>
+          <h6>{product.des}</h6>
           <div>
             <h3>Pris: {product.cost}</h3>
-            <Button style={{backgroundColor: "red", borderColor: "red", marginTop: "2%"}}> Köp </Button>
+            <Button style={{ backgroundColor: "red", borderColor: "red", marginTop: "2%" }}>Köp</Button>
           </div>
-        )}
-      </div>
-    </div>
+        </ProductInfo>
+      )}
+    </Container>
   );
 };
 
