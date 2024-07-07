ff89b93b12875548fb337a56df22f3c0a4f5cdbb
natalie.hallerdal@sensera.se <natalie.hallerdal@yahoo.se>
2024-07-07 15:56:12 +0200

    edit

--- src/pages/bookclubApply.jsx
+++ src/pages/bookclubApply.jsx
@@ -10,6 +10,25 @@ const BookClubApply = () => {
             <div>
             <input type="email" id="email" name="email" placeholder=" Email " style={{marginLeft: "2%",marginBottom:"1%", width: "30%" }}/>
             </div>
+            <div>
+            <input type="number" id="number-of-kids" placeholder=" Antal Barn" style={{marginLeft: "2%",marginBottom:"1%", width: "30%" }}/>
+            </div>
+
+            <div>
+            <label>
+                <input type="checkbox" name="option" value="option1" /> Nyckelpigorna
+            </label>
+            </div>
+            <div>
+            <label>
+                <input type="checkbox" name="option" value="option2" /> Björnarna
+            </label>
+            </div>
+            <div>
+            <label>
+                <input type="checkbox" name="option" value="option3" /> Fåglarna
+            </label>
+            </div>
         </form>
      </> 
     )
