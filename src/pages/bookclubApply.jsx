6a0239bc389caa050a8ecff6a961ff102f27e224
natalie.hallerdal@sensera.se <natalie.hallerdal@yahoo.se>
2024-07-07 16:15:15 +0200

    e

--- src/pages/bookclubApply.jsx
+++ src/pages/bookclubApply.jsx
@@ -6,14 +6,20 @@ const BookClubApply = () => {
 
         <form>
             <div>
-            <input type="text" id="name" name="name" placeholder=" Namn " style={{marginLeft: "2%", marginBottom:"1%",width: "30%" }}/>
+            <input type="text" id="name" name="name" placeholder=" Ditt Namn " style={{marginLeft: "2%", marginBottom:"1%",width: "30%" }}/>
             </div>
             <div>
-            <input type="email" id="email" name="email" placeholder=" Email " style={{marginLeft: "2%",marginBottom:"1%", width: "30%" }}/>
+            <input type="email" id="email" name="email" placeholder=" Din Email " style={{marginLeft: "2%",marginBottom:"1%", width: "30%" }}/>
             </div>
             <div>
             <input type="number" id="number-of-kids" placeholder=" Antal Barn" style={{marginLeft: "2%",marginBottom:"1%", width: "30%" }}/>
             </div>
+            <div>
+            <input type="text" id="name-kids" name="name-kids" placeholder=" Barn/Barnens namn (frivilligt) " style={{marginLeft: "2%",marginBottom:"1%", width: "30%" }}/>
+            </div>
+
+            
+
             <h5>Vilka eller vilken Bokklubb vill du anmäla dig till?</h5>
             <div>
             <label>
