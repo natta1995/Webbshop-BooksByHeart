1b92feab769d7e859babab87a37064aa2fbc23ec
natalie.hallerdal@sensera.se <natalie.hallerdal@yahoo.se>
2024-07-07 15:38:34 +0200

    edit

--- src/pages/bookclubApply.jsx
+++ src/pages/bookclubApply.jsx
@@ -1,6 +1,17 @@
 const BookClubApply = () => {
     return (
-        <h1>BookClub Apply</h1>
+    <>
+        <h1>Ansökan om medlemskap</h1>
+
+        <form>
+            <div>
+            <input type="text" id="name" name="name" placeholder=" Namn " style={{marginLeft: "2%", marginBottom:"1%",width: "30%" }}/>
+            </div>
+            <div>
+            <input type="email" id="email" name="email" placeholder=" Email " style={{marginLeft: "2%",marginBottom:"1%", width: "30%" }}/>
+            </div>
+        </form>
+     </> 
     )
 }
 
