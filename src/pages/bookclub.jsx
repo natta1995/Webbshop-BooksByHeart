9aee017a03b2b566f913f4861ae4a7f17aca864f
natalie.hallerdal@sensera.se <natalie.hallerdal@yahoo.se>
2024-07-16 15:12:00 +0200

    edit text

--- src/pages/bookclub.jsx
+++ src/pages/bookclub.jsx
@@ -10,6 +10,7 @@ const BookClub = () => {
             <div style={{ textAlign: "center", marginTop: "2%", marginBottom: "3%", backgroundColor: "#f0f0f0", padding: "2%"}}>
                 <h1 style={{fontFamily: "cursive"}}>Välkommen till vår bokklubbsida!</h1>
                 <h6 style={{fontFamily: "cursive"}} >Alla våra bokklubbar finns inte här ännu, hittar du inget som passar dig just nu så kommer fler att dyka upp under hösten. <br></br> Du kan alltid skicka ett meddelande till oss under kontakter, för mer information eller önskemål.</h6>
+                <p>Bra att veta! I varje erbjudande ingår två böcker i månaden, vid motagande av bok nummer 2 varje månad följer även en leksak med (som har stark koppling till boken som barnet får) Ifall ni redan äger boken i frågan har ni rätt att inom två veckor skicka tillbaka den och istället få en annan bok. (Observera att detta endast gäller 2 böcker om året.)</p>
             </div>
     
             <div style={{ display: "flex", alignItems: "center", marginBottom: "20px", backgroundColor:"#f0f0f0", padding: "1%" }}>
