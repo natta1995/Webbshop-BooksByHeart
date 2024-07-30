5e87a2b12e3e6eb786cd4183fe81ab6dd4b00cf9
natalie.hallerdal@sensera.se <natalie.hallerdal@yahoo.se>
2024-07-30 14:35:27 +0200

    edit bookclub

--- src/pages/bookclub.jsx
+++ src/pages/bookclub.jsx
@@ -1,25 +1,20 @@
+import { Link } from "react-router-dom";
 import img1 from "../img/reading smallest.jpg"
 import img2 from "../img/readingkid.jpg"
 import img3 from "../img/flera barn.jpg"
-import { Link } from "react-router-dom";
+
+
 
 const BookClub = () => {
     return (
         <>
-        
             <div style={{ textAlign: "center", marginTop: "2%", marginBottom: "3%", backgroundColor: "#f0f0f0", padding: "2%"}}>
                 <h1 style={{fontFamily: "cursive"}}>Välkommen till vår bokklubbsida!</h1>
                 <h6 style={{fontFamily: "cursive"}} >Alla våra bokklubbar finns inte här ännu, hittar du inget som passar dig just nu så kommer fler att dyka upp under hösten. <br></br> Du kan alltid skicka ett meddelande till oss under kontakter, för mer information eller önskemål.</h6>
             </div>
 
-            <div style={{ textAlign: "center", marginTop: "2%", marginBottom: "3%", backgroundColor: "#f0f0f0", padding: "2%"}}>
-            <p>Bra att veta! I varje erbjudande ingår två böcker i månaden, vid motagande av bok nummer 2 varje månad följer även en leksak med (som har stark koppling till boken som barnet får) Ifall ni redan äger boken i frågan har ni rätt att inom två veckor skicka tillbaka den och istället få en annan bok. (Observera att detta endast gäller 2 böcker om året.)</p>
-            </div>
-    
             <div style={{ display: "flex", alignItems: "center", marginBottom: "20px", backgroundColor:"#f0f0f0", padding: "1%" }}>
-                
                 <img src={img1} alt="babyreading" style={{ width: "40%", marginLeft: "20px" }}/>
-
                 <div style={{ flex: 1, marginLeft: "5%",  }}>
                     <h4>Nykelpigorna - Babygruppen</h4>
                     <p>Rekommenderad ålder: 0-3 år</p>
@@ -31,7 +26,6 @@ const BookClub = () => {
             </div>
     
             <div style={{ display: "flex", alignItems: "center", marginBottom: "20px", backgroundColor:"#f0f0f0", padding: "1%" }}>
-         
                 <img src={img2} alt="babyreading" style={{ width: "40%", marginLeft: "20px" }}/>
                 <div style={{ flex: 1, marginLeft: "5%" }}>
                     <h4>Björnarna - Nybörjarna</h4>
@@ -44,7 +38,6 @@ const BookClub = () => {
             </div>
     
             <div style={{ display: "flex", alignItems: "center", marginBottom: "20px" , backgroundColor:"#f0f0f0", padding: "1%"}}>
-             
                 <img src={img3} alt="babyreading" style={{ width: "40%", marginLeft: "20px" }}/>
                 <div style={{ flex: 1, marginLeft: "5%"  }}>
                     <h4>Fåglarna - Flerbarnsgänget</h4>
