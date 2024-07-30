5035f05f947c7debf2b397092776f12b94e9b243
natalie.hallerdal@sensera.se <natalie.hallerdal@yahoo.se>
2024-07-30 18:19:06 +0200

    adding 1 new db

---
+++ src/bookclubs.js
@@ -0,0 +1,25 @@
+const bookClubs = [
+    { 
+        id: 1, 
+        name: "Nyckelpigorna - Babygruppen",
+        price: "399:- / mån",
+        age: "0-3 år",
+        des: "Varannan tisdag kommer en ny bok i posten. Du kan när som helst välja att avsluta ditt medlemskap hos oss."
+    },
+    {
+        id: 2,
+        name: "Björnarna - Nybörjarna",
+        price: "399:- / mån",
+        age: "4-8 år",
+        des: "Varannan tisdag kommer en ny bok i posten. Du kan när som helst välja att avsluta ditt medlemskap hos oss."
+    },
+    {
+        id: 3,
+        name: "Fåglarna - Flerbarnsgänget",
+        price: "499:- / mån",
+        age: "0-12 år",
+        des: "Varannan tisdag kommer en ny bok i posten. Du kan när som helst välja att avsluta ditt medlemskap hos oss."
+    }
+]
+
+export default bookClubs;
