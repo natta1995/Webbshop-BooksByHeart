d905d1dde22e6aaeacd2a7a0ab1a61d4771379c3
natalie.hallerdal@sensera.se <natalie.hallerdal@yahoo.se>
2024-07-31 07:10:23 +0200

    bookklub db edit

--- src/bookclubs.js
+++ src/bookclubs.js
@@ -9,7 +9,7 @@ const bookClubs = [
         image: img1,
         price: "399:- / mån",
         age: "0-3 år",
-        des: "Varannan tisdag kommer en ny bok i posten. Du kan när som helst välja att avsluta ditt medlemskap hos oss."
+        des: "En gång i månaden kommer ett paket med tre stycken böcker och en leksak med stark förknippning till böckerna."
     },
     {
         id: 2,
@@ -17,7 +17,7 @@ const bookClubs = [
         image: img2,
         price: "399:- / mån",
         age: "4-8 år",
-        des: "Varannan tisdag kommer en ny bok i posten. Du kan när som helst välja att avsluta ditt medlemskap hos oss."
+        des: "Varannan tisdag kommer kommer en ny bok i brevlådan, varannan gång med en leksak starkt förknippat med boken. Att blanda läsning och lek tror vi ökar barnets intresse för läsning. Självklar ingår en hemlig välkomstgåva när du går med i denna bokklubb."
     },
     {
         id: 3,
@@ -25,7 +25,7 @@ const bookClubs = [
         image: img3,
         price: "499:- / mån",
         age: "0-12 år",
-        des: "Varannan tisdag kommer en ny bok i posten. Du kan när som helst välja att avsluta ditt medlemskap hos oss."
+        des: "Varannan torsdag kommer kommer en ny bok i brevlådan, varannan gång med en leksak starkt förknippat med boken. Att blanda läsning och lek tror vi ökar barnets intresse för läsning. Självklar ingår en hemlig välkomstgåva när du går med i denna bokklubb."
     }
 ]
 
